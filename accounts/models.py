c35d48ffcfeaf15023681a32931706d2adb61637
Harsh Mishra <hmisraji07@gmail.com>
2022-10-01 16:34:22 +0530

    Added models Hospital and PublicUser

--- accounts/models.py
+++ accounts/models.py
@@ -13,3 +13,21 @@ class User(AbstractUser):
 
     def __str__(self):
         return super().__str__()
+
+
+class Hospital(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(blank=False)
+    address = models.CharField(blank=False)
+    location = models.URLField(blank=False)
+
+    def __str__(self):
+        return str(user)
+
+
+class PublicUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(blank=False)
+
+
+
