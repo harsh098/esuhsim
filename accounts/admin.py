eb8ceebd81c18c0ca5e2d20285e39da7e2e2a0be
Harsh Mishra <hmisraji07@gmail.com>
2022-10-01 18:17:17 +0530

    Bug fix :: shifted to UserAdmin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.admin import UserAdmin as Useradmin
+from .models import User, Hospital, PublicUser
 from django.contrib import admin
 
-# Register your models here.
+
+class UserAdmin(Useradmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Permissions', {
+            'fields': ('is_staff', 'groups', 'user_permissions', 'is_hospital',),
+        }),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', "is_hospital",),
+
+        }),
+    )
+
+    list_display = ('username', 'email', 'is_staff')
+    list_filter = ('is_staff', 'groups')
+    search_fields = ('email', 'username')
+    ordering = ('username',)
+    filter_horizontal = ('groups', 'user_permissions')
+
+
+admin.site.register(User, UserAdmin)
+
+
+admin.site.register(Hospital)
+admin.site.register(PublicUser)
--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,40 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from .validators import validate_contact_number
 
 # Create your models here.
+
+
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model manager
+    """
+    def create_user(self, username, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        if not username:
+            raise ValueError('Blank Username not allowed')
+        if not email:
+            raise ValueError('The Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, email, password, **extra_fields):
+        """
+        Create and save a SuperUser
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(username, email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -11,14 +43,16 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     REQUIRED_FIELDS = ['email', 'ph_no']
 
+    objects = CustomUserManager()
+
     def __str__(self):
         return super().__str__()
 
 
 class Hospital(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(blank=False)
-    address = models.CharField(blank=False)
+    name = models.CharField(max_length=255, blank=False)
+    address = models.CharField(max_length=255, blank=False)
     location = models.URLField(blank=False)
 
     def __str__(self):
@@ -27,7 +61,7 @@ class Hospital(models.Model):
 
 class PublicUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(blank=False)
+    name = models.CharField(max_length=255, blank=False)
 
     def __str__(self):
         return str(self.user)
